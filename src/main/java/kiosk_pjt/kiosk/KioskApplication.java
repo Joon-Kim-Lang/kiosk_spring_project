28ac7c8f4f5bed5684a498da5b529301ec979022
H.J.Kim <khjohny95@yonsei.ac.kr>
2021-01-08 13:10:03 +0900

    original settings completed

---
+++ src/main/java/kiosk_pjt/kiosk/KioskApplication.java
@@ -0,0 +1,13 @@
+package kiosk_pjt.kiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kiosk_pjt/kiosk/KioskApplicationTests.java
@@ -0,0 +1,13 @@
+package kiosk_pjt.kiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
