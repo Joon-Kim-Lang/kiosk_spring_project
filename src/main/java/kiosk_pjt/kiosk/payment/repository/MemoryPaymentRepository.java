99c88cb1c0759104dbfd0841061d8506f16d58fd
Joon-Kim-Lang <khjohny95@yonsei.ac.kr>
2021-01-13 17:57:35 +0900

    jpaPayment initialized

--- src/main/java/kiosk_pjt/kiosk/payment/repository/MemoryPaymentRepository.java
+++ src/main/java/kiosk_pjt/kiosk/payment/repository/MemoryPaymentRepository.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Component
+
 public class MemoryPaymentRepository implements PaymentRepository{
 
     private static Map<String, PaymentInfo> store = new HashMap<>();
