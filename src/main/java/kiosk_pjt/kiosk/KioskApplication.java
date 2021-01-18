3ea328a36ec787ffca98b95395081da605cf150c
kks <keumks0331@gamil.com>
2021-01-18 17:41:29 +0900

    change KioskApplication

--- src/main/java/kiosk_pjt/kiosk/KioskApplication.java
+++ src/main/java/kiosk_pjt/kiosk/KioskApplication.java
@@ -9,11 +9,16 @@ import kiosk_pjt.kiosk.timetype.repository.JpaTimeTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@ServletComponentScan
 @SpringBootApplication
 public class KioskApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(KioskApplication.class, args);
+		SpringApplication application = new SpringApplication(KioskApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 }
